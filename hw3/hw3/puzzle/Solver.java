46099f16cb8bfad3e88922580b526b66f48465b8
chen-fang <eatappleonmars@gmail.com>
2017-08-02 11:52:39 -0500

    Finished coding part 1 with optimization and tested with WordPuzzleSolver class. Will move on to part 2 (Board) next

--- hw3/hw3/puzzle/Solver.java
+++ hw3/hw3/puzzle/Solver.java
@@ -2,28 +2,31 @@ package hw3.puzzle;
 
 import edu.princeton.cs.algs4.MinPQ;
 
-import java.util.Comparator;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 public class Solver {
-    WorldState _initial;
-    Set<WorldState> _solution;
+    SearchNode _initial;
 
     /** search node to support A* algorithm */
     private class SearchNode {
-        public WorldState state; /* a WorldState */
-        public int moves; /* the number of moves made to reach this world state from the initial state */
-        public SearchNode prev; /* a reference to the previous search node */
+        private WorldState state; /* a WorldState */
+        private int moves; /* the number of moves made to reach this world state from the initial state */
+        private SearchNode prev; /* a reference to the previous search node */
 
-        public SearchNode(WorldState state, int moves, SearchNode prev) {
+        /** Constuct SearchNode. prev is null */
+        public SearchNode(WorldState state, int moves ) {
             this.state = state;
             this.moves = moves;
-            this.prev = prev;
+            this.prev = null;
+        }
+
+        /** Set the prev node of the current node */
+        public void setPrev(SearchNode prevNode) {
+            this.prev = prevNode;
         }
     }
 
-    /** compare method for WorldState objects */
+    /** Method to compare SearchNodes */
     private class SearchNodeComparator implements Comparator<SearchNode> {
         @Override
         public int compare(SearchNode node1, SearchNode node2) {
@@ -38,11 +41,46 @@ public class Solver {
      *  puzzle using the A* algorithm. Assumes a solution exists.
      */
     public Solver(WorldState initial) {
-        // initialize the problem
-        this._initial = initial;
-        this._solution = new HashSet<>();
+        /* initialize the problem */
+        _initial = new SearchNode(initial, 0);
         MinPQ<SearchNode> queue = new MinPQ<>(new SearchNodeComparator());
-        queue.insert(new SearchNode(initial, 0, null));
+        queue.insert(_initial);
+
+        /* recursive call */
+        search(queue);
+    }
+
+    /** Search the path with A* algorithm
+     *  @param optPrev provides the parent SearchNode prior to the current search
+     *                 attempt for the purpose of optimization.
+     *  Optimization:  To reduce unnecessary exploration of useless search nodes,
+     *                 when considering the neighbors of a search node,
+     *                 don't enqueue a neighbor if its world state is the same as
+     *                 the world state of the previous search node.
+     */
+    private void search(MinPQ<SearchNode> queue, SearchNode optPrev) {
+        SearchNode minNode = queue.min();
+        queue.delMin();
+        if (minNode.state.isGoal())
+            return;
+        for (WorldState state : minNode.state.neighbors()) {
+            if (!state.equals(optPrev.state)) {
+                int moves = minNode.moves + 1;
+                queue.insert(new SearchNode(state, moves));
+            } else {
+                System.out.println("Optimization works");
+            }
+        }
+        minNode.prev = queue.min();
+        search(queue, minNode);
+    }
+
+    private void search(MinPQ<SearchNode> queue) {
+        /* optPrev should be initialized with null intuitively, but I chose to use
+         * queue.min() instead because it helps avoid repeated codes and nested
+         * if-statements caused by null.
+         */
+        search(queue, queue.min());
     }
 
     /** Returns the minimum number of moves to solve the puzzle starting at the
@@ -50,12 +88,18 @@ public class Solver {
      *
      */
     public int moves() {
-        return _initial.estimatedDistanceToGoal();
+        return _initial.state.estimatedDistanceToGoal();
     }
 
     /** Returns a sequence of WorldStates FROM the initial WorldState to the solution */
     public Iterable<WorldState> solution() {
-        return _solution;
+        List<WorldState> path = new ArrayList<>();
+        SearchNode p = _initial;
+        while (p != null) {
+            path.add(p.state);
+            p = p.prev;
+        }
+        return path;
     }
 
     public static void main(String[] args) {
